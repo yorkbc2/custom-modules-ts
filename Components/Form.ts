b10bfb79bd3c22b01065000a977bcc49806181f8
Alex Yorke <yorkbc2@gmail.com>
2018-07-06 10:41:58 +0300

    New Features added to Form.ts

--- Components/Form.ts
+++ Components/Form.ts
@@ -1,82 +1,55 @@
 import {
 	sendPromisedRequest
 } from "./../Modules/AjaxModule";
-// import {
-// 	inlineCss,
-// 	create,
-// 	attachEvent
-// } from './../Modules/SugarDOM'
-import {
-	FormInterface,
-	FormPattern
-} from "./FormInterfaces"
 
-function addInputEvent (elements, data:Object) {
-	if (elements) {
-		elements.forEach(i => {
-			if ('addEventListener' in i) {
-				if (i.name !== "") {
-					data[i.name] = i.value
-				}
-				i.addEventListener('input', inputEvent.bind(this, data));
-			}
-		})
-	}
+interface FormInterface {
+	container: Element|null;
+	data: Object;
+	SQLInjectionRegular: RegExp;
+	successCallback: Function|null;
+	errorCallback: Function|null;
+	maxSymbolsLengthForInputs: number;
+	maxSymbolsLengthForTextareas: number;
 }
 
-function inputEvent (data:Object, event: any) {
-	if (event.target.name !== "")
-		data[event.target.name] = event.target.value;
+interface FormPattern {
+	name: string;
+	pattern: RegExp;
 }
 
-function usePattern (pattern: FormPattern, value: string): boolean {
-	if (pattern.pattern.test) {
-		return pattern.pattern.test(value);
-	} else {
-		return false;
-	}
+interface FormOptions {
+	maxLengthInputs: number|null;
+	maxLengthTextareas: number|null;
 }
 
-function checkPattern (input: HTMLInputElement, patternList: FormPattern[]):boolean {
-	var currentPattern: FormPattern; 
-	var dataPattern: string = input.getAttribute('data-pattern') || '';
-	if (dataPattern === null || dataPattern === '') {
-		return true;
-	} else {
-
-		currentPattern = patternList.filter(p => p.name === dataPattern)[0];
-
-		if (currentPattern) {
-
-			return usePattern(currentPattern, input.value)
-
-		} else {
-		 	return false;
-		}
-
-	}
+interface CheckInputLengthOptions {
+	maxInputs: number;
+	maxAreas: number;
 }
-
 
 class Form implements FormInterface {
 	container: Element|null;
 	data: Object = {};
 	successCallback: Function|null = null;
 	errorCallback: Function|null = null;
-	// errorPopupMessage: Element|null = null;
-	// errorMessage: string = 'Это поле заполнено неправильно! Оно пустое либо не соответствует шаблону ввода.';
+	invalidInputCallback: Function|null = null;
+	maxSymbolsLengthForInputs: number = 24;
+	maxSymbolsLengthForTextareas: number = 360;
 	patterns: FormPattern[] = [
 		{
 			name: "phone",
 			pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
 		}
 	];
-	beforeSendCallback: Function = function () {
-		this.container.classList.add('form-loading');
-	}
-	constructor(selector: string) {
+	SQLInjectionRegular: RegExp = /<[^>]{0,}[^/]>/g;
+	
+	constructor(selector: string, options: FormOptions|null) {
 		this.container = document.querySelector(selector) || null;
-		
+		if (options) {
+			this.maxSymbolsLengthForInputs = options.maxLengthInputs || this.maxSymbolsLengthForInputs;
+			this.maxSymbolsLengthForTextareas = options.maxLengthTextareas || this.maxSymbolsLengthForTextareas;
+		}
+
 		if (this.container) {
 			var inputs: NodeListOf<HTMLInputElement> = this.container.querySelectorAll('input, select, textarea');
 			if (inputs && inputs.length > 0) {
@@ -86,19 +59,25 @@ class Form implements FormInterface {
 		}
 	}
 
+	beforeSendCallback: Function = function () {
+		this.container.classList.add('form-loading');
+	}
+
 	submit(event:any) {
 		event.preventDefault();
 		
 		var action:string = event.target.getAttribute('action');
 		var method:string = event.target.getAttribute('method') || "POST";
 		var isPrepared = true;
-		// this.removeErrorPopupMessage();
 		for (var i in this.data) {
 			var input: HTMLInputElement = this.container.querySelector(`input[name="${i}"],select[name="${i}"]`);
-			if (this.data[i] === "" || checkPattern(input, this.patterns) === false) {
+			if (this.data[i] === "" || checkPattern(input, this.patterns) === false || checkInputLength(input, {
+				maxInputs: this.maxSymbolsLengthForInputs,
+				maxAreas: this.maxSymbolsLengthForTextareas
+			}) === false) {
 				isPrepared = false;
 				if (input.className.indexOf("form-error") == -1) input.className = "form-error";
-				// this.createErrorPopupMessage();
+				if (typeof this.invalidInputCallback === 'function') this.invalidInputCallback(input, this.container);
 				break;
 			} else {
 				input.className = input.className.replace("form-error", "").trim()
@@ -137,29 +116,72 @@ class Form implements FormInterface {
 		return this;
 	}
 
-	// setErrorMessage (message:string) {
-	// 	this.errorMessage = message;
-	// }
+	setInvalidInputCallback (callback: Function) {
+		if (typeof callback === 'function') this.invalidInputCallback = callback;
+		else this.invalidInputCallback = null;
+		return this;
+	}
 
-	// removeErrorPopupMessage () {
-	// 	if (this.errorPopupMessage !== null) {
-	// 		this.errorPopupMessage.parentNode.removeChild(this.errorPopupMessage);
-	// 		this.errorPopupMessage = null;
-	// 	}
-	// }
+}
 
-	// createErrorPopupMessage () {
-	// 	if (this.errorPopupMessage === null) {
-	// 		this.errorPopupMessage = create('div');
-	// 		this.errorPopupMessage.textContent = this.errorMessage;
-	// 		this.errorPopupMessage.className = "form-error-popup-message";
-	// 		attachEvent(<HTMLElement>this.errorPopupMessage, 'click', () => {
-	// 			this.removeErrorPopupMessage();
-	// 		})
-	// 		this.container.appendChild(this.errorPopupMessage);
-	// 	}
-	// }
+function addInputEvent (elements, data:Object) {
+	if (elements) {
+		elements.forEach(i => {
+			if ('addEventListener' in i) {
+				if (i.name !== "") {
+					data[i.name] = i.value
+				}
+				i.addEventListener('input', inputEvent.bind(this, data));
+			}
+		})
+	}
+}
 
+function inputEvent (data:Object, event: any) {
+	if (event.target.name !== "")
+		data[event.target.name] = event.target.value.replace(this.SQLInjectionRegular, '');
+}
+
+function usePattern (pattern: FormPattern, value: string): boolean {
+	if (pattern.pattern.test) {
+		return pattern.pattern.test(value);
+	} else {
+		return false;
+	}
+}
+
+function checkPattern (input: HTMLInputElement, patternList: FormPattern[]):boolean {
+	var currentPattern: FormPattern; 
+	var dataPattern: string = input.getAttribute('data-pattern') || '';
+	if (dataPattern === null || dataPattern === '') {
+		return true;
+	} else {
+
+		currentPattern = patternList.filter(p => p.name === dataPattern)[0];
+
+		if (currentPattern) {
+
+			return usePattern(currentPattern, input.value)
+
+		} else {
+		 	return false;
+		}
+
+	}
+}
+
+function checkInputLength (input: HTMLInputElement, options: CheckInputLengthOptions) {
+	switch (input.tagName) {
+		case "INPUT": {
+			return input.value.length > options.maxInputs? false: true;
+		}
+		case "TEXTAREA": {
+			return input.value.length > options.maxAreas? false: true;
+		}
+		default: {
+			return false;
+		}
+	}
 }
 
 export default Form;
--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import Modal from './Components/Modal';
 import Form from './Components/Form';
 import './Modules/AjaxModule';
 import './Modules/SugarDOM';
-import './Components/FormInterfaces';
 
 (function () {
 	(<any>window).Modal = Modal;
